0f03817efb35f533ea4ebd6aecb3776ef3068444
Emily Liu <emilyliu@Emilys-MacBook-Air.local>
2023-10-02 15:12:31 +1100

    IM DONE

--- src/pages/Facts.jsx
+++ src/pages/Facts.jsx
@@ -11,50 +11,26 @@ import { catFacts, catPics } from '../assets/cats'
 const Facts = () => {
   // Put main logic/hooks here 
 
-  // State variable to keep track of the cat fact being displayed
+  // TODO: Make a state variable to keep track of the cat fact being displayed
   // catFact holds the current fact (string) being shown, setCatFact changes this 
-  const [catFact, setCatFact] = useState(null); 
   
-  // State variable to keep track of the current index of the catPic array
-  const [picNumber, setPicNumber] = useState(0);
+  // TODO: Make a state variable to keep track of the current index of the catPic array
   
-  // State variable to check if fact button has been clicked - to keep track of when to change the button label 
-  const [buttonClicked, setButtonClicked] = useState(false);
-  
-  // Runs whenever fact button is clicked 
+  // TODO: Make a state variable to check if fact button has been clicked - to keep track of when to change the button label   
+  // TODO: Complete this function (will run whenever we want the button to be clicked)
   function handleClick() {
-    // Change button clicked to true (which changes the label)
-    if(!buttonClicked) setButtonClicked(true);
+    // TODO: Change button clicked to true (which changes the label)
     
-    // Pick a random decimal between 0.0 and 1.0, multiply it by length of catFacts array (0.0-19.0)
-    // and then round it down to nearest whole number (Math.floor())
-    const randIndex = Math.floor(Math.random() * catFacts.length);
-    // sets the resulting index (whole number between 0-19)
-    setCatFact(catFacts[randIndex]);
+    // TODO: Pick a random decimal between 0.0 and 1.0, multiply it by length of catFacts array (0.0-19.0) and then round it down to nearest whole number (hint: Math library)
 
-    // Show the next cat picture in order if we aren't about to hit the end of the array
-    if (picNumber < 19) {
-      // increments upon the previous value (denoted by prev)
-      // we dont do setPicNumber(picNumber + 1) because it causes issues
-      setPicNumber(prev => prev + 1)
-    } else {
-      // if we are abt to hit end array, reset index to 0 (first pic)
-      setPicNumber(0)
-    }
-  }
+    // TODO: Set the current cat fact to the fact at random index (from before)
 
+    // TODO: Show the next cat picture in order if we aren't about to hit the end of the array    }
+
+}
   return (
-    // Put JSX here
+    // TODO: Put JSX here
     <div className='container'>
-      <div className='img-container'>
-        <img src={catPics[picNumber]} alt='cat pic'/>
-      </div>
-      <Button
-        onClick={handleClick}
-      >
-        {buttonClicked ? 'Another one 🗿' : 'Press to get a cool cat fact!'}
-      </Button>
-      <h1 className='cat-fact'>{catFact}</h1>
     </div>
   )
 }
